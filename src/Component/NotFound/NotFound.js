1c28c0b9d381ac1b1bd95c47e16e5600859b604f
ibntofajjal <joynal@bintofajjal.info>
2021-11-08 18:02:46 +0600

    404 Page designed

--- src/Component/NotFound/NotFound.js
+++ src/Component/NotFound/NotFound.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
     <div>
-      <h1 style={{ marginTop: "5rem" }}>Website Not Found</h1>
+      <h1 style={{ marginTop: "6rem" }}></h1>
+      <div className="not-found-page">
+        <Link to="/home">
+          <button className="back2home">
+            {" "}
+            <BiArrowBack />
+            Back To Home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
